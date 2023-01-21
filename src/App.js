a548a286fcc6b505918569b9ad103b2db649fb55
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2023-01-21 15:52:35 -0800

    Fixed React Script vers. and Added Navbar and Box.
    Co-authored-by: Jay Park <jpkani@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-
 import './App.css';
 import React from 'react';
-import NavButton from './components/NavButton';
+import Setup from './components/Setup';
 import { useState, useEffect } from 'react';
+import { PhoneIcon } from '@chakra-ui/icons';
 
 function App() {
   return (
     <div className="App">
+      <PhoneIcon/>
+       <img id="backgroundImg" src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"/>
       <div class="container">
         <div class="navbar">
           <div class="firstnav">
-            <image src="https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/1608506169128-S6KYNEV61LEP5MS1UIH4/traditional-food-around-the-world-Travlinmad.jpg"/>
-
+            <img src="./assets/Time.png" alt="Logo"/>
           </div>
-          <NavButton
-            text="About Us"
-            link=""
-          />
-
-
-
+          <div class="nav-btns">
+            <div class="nav-button">
+              <h4 id="nav-text">Let's Eat!</h4>
+            </div>
+            <div class="nav-button">
+              <h4 id="nav-text">About Us</h4>
+            </div>
+          </div>
         </div>
-
-
-
+        <Setup/>
       </div>
+     
     </div>
   );
 }
 
+
 export default App;
+
