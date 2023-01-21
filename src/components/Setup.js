3f2f17238f2466019338196b57e52b0c5c5cb16f
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2023-01-21 15:54:43 -0800

    Added: - Navbar - Box - Updated ReactScripts
    In collab with: jpkani

---
+++ src/components/Setup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Setup.css';
+import { Progress } from '@chakra-ui/icons';
+
+
+function Setup() {
+    return(
+        <div id="setupbox">
+            
+        </div>
+    )
+}
+export default Setup;
