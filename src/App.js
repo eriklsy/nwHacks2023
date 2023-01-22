043222b24d9821018e083af70c6eda8dc8bb23f8
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2023-01-21 17:51:07 -0800

    FIXED UI
    Co-authored-by: Jay Park <jpkani@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import './App.css';
 import React from 'react';
 import Setup from './components/Setup';
+import P1 from './components/P1';
 import { useState, useEffect } from 'react';
-import { PhoneIcon } from '@chakra-ui/icons';
+import { Box, HStack } from '@chakra-ui/react';
+import logo from './assets/Time.png';
 
 function App() {
   return (
     <div className="App">
-      <PhoneIcon/>
-       <img id="backgroundImg" src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"/>
+       <img id="backgroundImg" src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80" alt="background-img"/>
       <div class="container">
         <div class="navbar">
           <div class="firstnav">
-            <img src="./assets/Time.png" alt="Logo"/>
+            <a href=""><img src={logo} alt="logo" /></a>
           </div>
           <div class="nav-btns">
             <div class="nav-button">
-              <h4 id="nav-text">Let's Eat!</h4>
+              <h4 id="first-nav">Let's Eat!</h4>
             </div>
             <div class="nav-button">
-              <h4 id="nav-text">About Us</h4>
+              <h4 id="second-nav">About Us</h4>
             </div>
           </div>
         </div>
+        <div class="title">
+          It's Time to Eat!
+        </div>
         <Setup/>
       </div>
-     
     </div>
   );
 }
