01cbaefd4664027029115f85704f1d73cbc7bad2
TheIceCreamBoss <nicholaszhang0817@gmail.com>
2023-01-21 23:14:09 -0800

    removed box when getting to images

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -23,78 +23,92 @@ function Setup() {
     const [isShown, setIsShown] = useState(false);
     const handleClick = event => {
        console.log (isShown);
+        handleBoxClick();
         setIsShown(current => !current);
+
+    }
+
+    const [showBox, setBoxShown] = useState(true);
+    const handleBoxClick = event => {
+       console.log (showBox);
+        setBoxShown(current => !current);
     }
 
 
-
+    let elementStyle = {};
+    if (!showBox) {
+    elementStyle = { display: 'none' };
+  }
 
     return(
         <div>
-        <Box id="setupbox">
-            <div>
-                <button onClick={handleSliderChange} id="next-button">
-                    <img src={poop} alt="next button"/>
-                </button>
-            </div>
-            <div>
-                <Tabs 
-                align="center" 
-                isFitted variant='enclosed'
-                onChange={handleTabsChange}
-                index={tabIndex}>
-                    <TabList>
-                        <div className='listof'>   
-                            <Tab className="items">
-                            </Tab>
-                            <Tab className='items'>
-                            </Tab>
-                            <Tab className='items'>
-                            </Tab>
-                            <Tab className='items'>
-                            </Tab>
-                        </div>
-                    </TabList>
-                    <TabPanels className="alltabs">
-                        <TabPanel>
-                        <div className='tab1'>
-                            <h3>
-                                how many eaters?
-                            </h3>
-                        </div>
-                        </TabPanel>
-                        <TabPanel>
-                        <div className='tab2'>
-                            <h3>
-                                what's the price range?
-                            </h3>
-                        </div>
-                        </TabPanel>
-                        <TabPanel>
-                        <div className='tab3'>
-                            <h3>
-                                where are we eating?
-                            </h3>
-                            <IconButton onClick={function() {handleClick()}} className="finalBtn"
-                                colorScheme='blue'
-                                aria-label='Search database'
-                                size='lg'
-                                icon={<ArrowRightIcon/>}/>
-
-                            
-                        </div>
-                        </TabPanel>
-                        <TabPanel>
-                        <div className='tab4'>
-                            
-                        </div>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-                
-            </div>
+            <div style={!showBox ? {display: 'none'} : {}}>
+            <Box id="setupbox">
             
-        </Box>
+                <div>
+                    <button onClick={handleSliderChange} id="next-button">
+                        <img src={poop} alt="next button"/>
+                    </button>
+                </div>
+                <div>
+                    <Tabs 
+                    align="center" 
+                    isFitted variant='enclosed'
+                    onChange={handleTabsChange}
+                    index={tabIndex}>
+                        <TabList>
+                            <div className='listof'>   
+                                <Tab className="items">
+                                </Tab>
+                                <Tab className='items'>
+                                </Tab>
+                                <Tab className='items'>
+                                </Tab>
+                                <Tab className='items'>
+                                </Tab>
+                            </div>
+                        </TabList>
+                        <TabPanels className="alltabs">
+                            <TabPanel>
+                            <div className='tab1'>
+                                <h3>
+                                    how many eaters?
+                                </h3>
+                            </div>
+                            </TabPanel>
+                            <TabPanel>
+                            <div className='tab2'>
+                                <h3>
+                                    what's the price range?
+                                </h3>
+                            </div>
+                            </TabPanel>
+                            <TabPanel>
+                            <div className='tab3'>
+                                <h3>
+                                    where are we eating?
+                                </h3>
+                                <IconButton onClick={function(){handleClick()}} className="finalBtn"
+                                    colorScheme='blue'
+                                    aria-label='Search database'
+                                    size='lg'
+                                    icon={<ArrowRightIcon/>}/>
+
+                                
+                            </div>
+                            </TabPanel>
+                            <TabPanel>
+                            <div className='tab4'>
+                                
+                            </div>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                    
+                </div>
+                
+            </Box>
+            </div>
         {isShown && <FoodSelection/>}
         </div>
     );
