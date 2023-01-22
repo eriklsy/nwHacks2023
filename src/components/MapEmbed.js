1a550add5cd858cebd9ff85e6a66c8111a7eccfa
eriklsy <erik.s.lin.2011@gmail.com>
2023-01-21 22:07:02 -0800

    added marker

--- src/components/MapEmbed.js
+++ src/components/MapEmbed.js
@@ -1,55 +1,46 @@
-import React, { Component, useState} from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React from "react"
 
-import Geocode from "react-geocode";
-const mapStyles = {
-  width: '25%',
-  height: '85%'
-};
-Geocode.setApiKey("AIzaSyA6MTpfVrCDiXw_JY5mBcd3Rjk0OdHi0VI");
-
-export class MapContainer extends Component {
-  constructor(props){
-    super(props);
-  
-  }
-  state ={
-    city: "Vancouver",
-    latitude: 49.2827291,
-    longitude: -123.1207375,
-    selectedLat: 49.2827291,
-  }
-  componentDidMount() {
-    console.log(this.props.city)
-   /* Geocode.fromAddress(this.props.city).then(
-      (response) => {
-        const { lat, lng } = response.results[0].geometry.location;
-       console.log(lat);
-       console.log(lng);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );*/
-  }
-  render() {  
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: this.state.latitude,
-            lng: this.state.longitude
-          }
-        }
-      />
-    
-    );
-  }
+class MapWithAMarker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 49.2827291,
+            lng: -123.1207375
+        };
+    }
+    handleMapClick = (event) => {
+        this.setState({
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+        });
+    }
+    render() {
+        return (
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: this.state.lat, lng: this.state.lng }}
+                onClick={this.handleMapClick}
+            >
+                <Marker
+                    position={{ lat: this.state.lat, lng: this.state.lng }}
+                />
+            </GoogleMap>
+        );
+    }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyA6MTpfVrCDiXw_JY5mBcd3Rjk0OdHi0VI'
-})(MapContainer);
+const WrappedMapWithAMarker = withScriptjs(withGoogleMap(MapWithAMarker));
+
+function MapContainer() {
+    return (
+        <WrappedMapWithAMarker
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6MTpfVrCDiXw_JY5mBcd3Rjk0OdHi0VI&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+    );
+}
+
+export default MapContainer;
